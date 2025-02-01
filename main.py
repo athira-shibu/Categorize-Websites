e4d3569763de5079e0a094054ea56c0926ea4efe
athira-shibu <athira.shibu@dibasys.com>
2025-02-01 09:39:20 +0530

    beautiful soup

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from bs4 import BeautifulSoup
 with open('home.htm', 'r') as html_file:
     content = html_file.read()
     print(content)
+
+soup = BeautifulSoup(content, 'lxml')
+print(soup.prettify())
